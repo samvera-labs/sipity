9d32641f899b8d586cb5cd14a50e03a9a7322378
Justin Coyne <jcoyne@justincoyne.com>
2018-02-28 09:25:54 -0600

    Generators must end in _generator.rb

--- lib/generators/sipity/install_generator.rb
+++ lib/generators/sipity/install_generator.rb
@@ -5,5 +5,10 @@ module Sipity
   # for engine cart
   class Install < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
+
+    # Setup the database migrations
+    def copy_migrations
+      rake "sipity:install:migrations"
+    end
   end
 end
