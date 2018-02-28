b66a274c5c4056e48ae850146a332f63a2a0529e
Justin Coyne <jcoyne@justincoyne.com>
2018-02-28 08:56:45 -0600

    Flesh out Sipity::Role

--- db/migrate/20180228145201_create_sipity_roles.rb
+++ db/migrate/20180228145201_create_sipity_roles.rb
@@ -1,8 +1,11 @@
 class CreateSipityRoles < ActiveRecord::Migration[5.1]
   def change
     create_table :sipity_roles do |t|
-
+      t.string   'name', null: false
+      t.text     'description'
       t.timestamps
     end
+
+    add_index 'sipity_roles', ['name'], name: 'index_sipity_roles_on_name', unique: true
   end
 end
